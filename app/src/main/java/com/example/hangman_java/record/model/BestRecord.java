9f8a0e64aa6f15c9a624934d3d4eb0a3de187667
EveryPine <starcraft819@gmail.com>
2023-10-28 15:25:15 +0900

    BestRecord 테이블 구조 수정

--- app/src/main/java/com/example/hangman_java/record/model/BestRecord.java
+++ app/src/main/java/com/example/hangman_java/record/model/BestRecord.java
@@ -1,19 +1,36 @@
 package com.example.hangman_java.record.model;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Entity(tableName = "BestRecord")
 public class BestRecord {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public int record_id;
-    @ColumnInfo(name="gamename")
+    @ColumnInfo(name="gamename") @NonNull
     public String game;
-    @ColumnInfo(name="difficulty")
+    @ColumnInfo(name="difficulty") @NonNull
     public String difficulty;
     @ColumnInfo(name="record")
     public int record;
-    @ColumnInfo(name="date")
+    @ColumnInfo(name="date") @NonNull
     public String date;
+
+    public BestRecord(int record_id, @NonNull String game, @NonNull String difficulty, int record){
+        this.record_id = record_id;
+        this.game = game;
+        this.difficulty = difficulty;
+        this.record = record;
+        Date date = new Date((long) System.currentTimeMillis());
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        this.date = dateFormat.format(date);
+    }
 }
