563844906574e2da253cdcc024826010532e20e2
EveryPine <starcraft819@gmail.com>
2023-11-02 21:18:10 +0900

    배경음, 효과음 설정 버그 해결

--- app/src/main/java/com/example/hangman_java/main/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/hangman_java/main/viewmodel/MainViewModel.java
@@ -1,56 +1,76 @@
 package com.example.hangman_java.main.viewmodel;
 
+import static com.example.hangman_java.main.view.MainActivity.mainActivity;
+
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.hangman_java.base.AppDatabase;
 import com.example.hangman_java.base.BaseViewModel;
 import com.example.hangman_java.music.BgmService;
 
 public class MainViewModel extends BaseViewModel {
-    private final MutableLiveData<Integer> _effectVolume = new MutableLiveData<>(10);
-    private final MutableLiveData<Integer> _backgroundVolume = new MutableLiveData<>(10);
-    private final MutableLiveData<Boolean> _bgmMuted = new MutableLiveData<>(false);
-    private final MutableLiveData<Boolean> _eftMuted = new MutableLiveData<>(false);
+    private SharedPreferences preferences;
+    private final MutableLiveData<Integer> _bgmVolume = new MutableLiveData<>();
+    private final MutableLiveData<Integer> _eftVolume = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> _bgmMuted = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> _eftMuted = new MutableLiveData<>();
+    private void getSharedPreferences(){ if (preferences==null) preferences = mainActivity.getSharedPreferences("UserInfo", Context.MODE_PRIVATE); }
+
+    public void setUserInfo(){
+        getSharedPreferences();
+        Log.d("MyTAG", "setUserInfo: " + preferences.getBoolean("bgmMuted", false) + " " + preferences.getBoolean("eftMuted", false));
+        _bgmVolume.setValue(preferences.getInt("bgmVolume", 10));
+        _eftVolume.setValue(preferences.getInt("eftVolume", 10));
+        _bgmMuted.setValue(preferences.getBoolean("bgmMuted", false));
+        _eftMuted.setValue(preferences.getBoolean("eftMuted", false));
+        Log.d("MyTAG", "_bgmMuted: " + getBgmMuted());
+    }
 
     public void setBackgroundVolume(Context context, int progress){
         Intent intent = new Intent(context, BgmService.class);
         intent.putExtra("progress", progress);
         context.startService(intent);
         _bgmMuted.setValue(false);
-        _backgroundVolume.setValue(progress);
+        _bgmVolume.setValue(progress);
     }
 
     public void setBgmVolumeMuted(Context context){
         Intent intent = new Intent(context, BgmService.class);
         intent.putExtra("progress", 0);
         context.startService(intent);
-        Log.d("MyTAG", "bgmMuted 변경전 : " + _bgmMuted.getValue());
         _bgmMuted.setValue(true);
-        Log.d("MyTAG", "bgmMuted 변경됨 : " + _bgmMuted.getValue());
     }
 
     public void setEffectVolume(int progress){
         _eftMuted.setValue(false);
-        _effectVolume.setValue(progress);
+        _eftVolume.setValue(progress);
     }
-    public void setEffectVolumeMuted(Context context){
+    public void setEffectVolumeMuted(){
         _eftMuted.setValue(true);
-        Intent intent = new Intent(context, BgmService.class);
-        intent.putExtra("progress", 0);
-        context.startService(intent);
     }
 
-    public int getBackgroundVolume(){
-        return _backgroundVolume.getValue();
-    }
+    public int getBgmVolume(){ return _bgmVolume.getValue(); }
 
-    public int getEffectVolume(){
-        return _effectVolume.getValue();
-    }
+    public int getEftVolume(){ return _eftVolume.getValue(); }
 
-    public boolean isBgmMuted() { return _bgmMuted.getValue(); }
-    public boolean isEftMuted() { return _eftMuted.getValue(); }
+    public boolean getBgmMuted() { return _bgmMuted.getValue(); }
+
+    public boolean getEftMuted() { return _eftMuted.getValue(); }
+
+    public void updateUserInfo(){
+        getSharedPreferences();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("bgmVolume", getBgmVolume());
+        editor.putInt("eftVolume", getEftVolume());
+        editor.putBoolean("bgmMuted", getBgmMuted());
+        editor.putBoolean("eftMuted", getEftMuted());
+        editor.apply();
+        Log.d("MyTAG", "updateUserInfo: " + preferences.getBoolean("bgmMuted", false) + " " + preferences.getBoolean("eftMuted", false));
+    }
 }
--- app/src/main/java/com/example/hangman_java/music/BgmService.java
+++ app/src/main/java/com/example/hangman_java/music/BgmService.java
@@ -26,16 +26,18 @@ public class BgmService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-        if (!mediaPlayer.isPlaying())
+        if (!mediaPlayer.isPlaying()){
             mediaPlayer.start();
-        setBackgroundVolume(intent.getIntExtra("progress", 10));
-        Log.d("MyTAG", "배경음악이 시작됨");
+            Log.d("MyTAG", "배경음악이 시작됨");
+        }
+        this.setBackgroundVolume(intent.getIntExtra("progress", 10));
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void setBackgroundVolume(int progress){
         float output = progress / 10.0f;
         mediaPlayer.setVolume(output, output);
+        Log.d("MyTAG", "배경음악 볼륨 조절됨 (progress: " + progress + ")");
     }
 
     public void onDestroy(){
